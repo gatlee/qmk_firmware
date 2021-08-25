adff545e5b863b2700af158f77699252e7345cc1
Gatlee Kaw <gatlee.kaw@gmail.com>
2021-08-25 17:29:58 +1000

    Update keymap with correct modifiers and thumb keys

---
+++ keyboards/crkbd/keymaps/gatlee/rules.mk
@@ -0,0 +1,31 @@
+# copied from users/manna-harbour_miryoku/rules.mk
+# Copyright 2019 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+# generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
+
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = yes # Auto Shift
+
+# alternative layouts:
+
+# alphas
+OPT_DEFS += -DMIRYOKU_ALPHAS_QWERTY
+
+# nav
+OPT_DEFS += -DMIRYOKU_NAV_VI
+
+# clipboard
+ifneq ($(strip $(MIRYOKU_CLIPBOARD)),)
+  OPT_DEFS += -DMIRYOKU_CLIPBOARD_$(MIRYOKU_CLIPBOARD)
+endif
+
+# layers
+ifneq ($(strip $(MIRYOKU_LAYERS)),)
+  OPT_DEFS += -DMIRYOKU_LAYERS_$(MIRYOKU_LAYERS)
+endif
+
+# subset mappings
+ifneq ($(strip $(MIRYOKU_MAPPING)),)
+  OPT_DEFS += -DMIRYOKU_MAPPING_$(MIRYOKU_MAPPING)
+endif
