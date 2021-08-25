c2c6468bfc03ada76a8be8925aae5799cdd79daf
Gatlee Kaw <gatlee.kaw@gmail.com>
2021-08-25 21:05:13 +1000

    Invert numpad

--- keyboards/crkbd/keymaps/gatlee/keymap.c
+++ keyboards/crkbd/keymaps/gatlee/keymap.c
@@ -79,23 +79,22 @@ U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,   U_RDO,   U_PST,   U_CPY,   U_CUT,  
 U_NP,    U_NP,    KC_BTN2, KC_BTN3, KC_BTN1, KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
 ),
 [NUM] = LAYOUT_miryoku(
-KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+KC_LBRC, KC_1,    KC_2,    KC_3,    KC_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
 KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,  U_NA,    KC_LGUI, KC_LSFT, KC_LALT, KC_LCTL,
-KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
+KC_GRV,  KC_7,    KC_8,    KC_9,    KC_BSLS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
 U_NP,    U_NP,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
 ),
 [SYM] = LAYOUT_miryoku(
-KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+KC_LCBR, KC_EXLM, KC_AT,   KC_HASH, KC_RCBR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
 KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, U_NA,    KC_LGUI, KC_LSFT, KC_LALT, KC_LCTL,
-KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
+KC_TILD, KC_AMPR, KC_ASTR, KC_LPRN, KC_PIPE, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
 U_NP,    U_NP,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
 ),
 [FUN] = LAYOUT_miryoku(
-KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_PSCR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
 KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK, U_NA,    KC_LGUI, KC_LSFT, KC_LALT, KC_LCTL,
-KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
+KC_F10,  KC_F7,   KC_F8,   KC_F9,   KC_PAUS, U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
 U_NP,    U_NP,    KC_APP,  KC_SPC,  KC_TAB,  U_NA,    U_NA,    U_NA,    U_NP,    U_NP
 )
 };
-
 
