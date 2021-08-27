67910f2c5fe1ef716cde7b7b2c23468581ea0078
Gatlee Kaw <gatlee.kaw@gmail.com>
2021-08-27 20:39:35 +1000

    Remove optional stuff

--- keyboards/crkbd/keymaps/gatlee/rules.mk
+++ keyboards/crkbd/keymaps/gatlee/rules.mk
@@ -9,26 +9,3 @@ AUTO_SHIFT_ENABLE = yes # Auto Shift
 OLED_DRIVER_ENABLE = yes
 WPM_ENABLE = yes
 LTO_ENABLE = yes
-
-# alternative layouts:
-
-# alphas
-OPT_DEFS += -DMIRYOKU_ALPHAS_QWERTY
-
-# nav
-OPT_DEFS += -DMIRYOKU_NAV_VI
-
-# clipboard
-ifneq ($(strip $(MIRYOKU_CLIPBOARD)),)
-  OPT_DEFS += -DMIRYOKU_CLIPBOARD_$(MIRYOKU_CLIPBOARD)
-endif
-
-# layers
-ifneq ($(strip $(MIRYOKU_LAYERS)),)
-  OPT_DEFS += -DMIRYOKU_LAYERS_$(MIRYOKU_LAYERS)
-endif
-
-# subset mappings
-ifneq ($(strip $(MIRYOKU_MAPPING)),)
-  OPT_DEFS += -DMIRYOKU_MAPPING_$(MIRYOKU_MAPPING)
-endif
